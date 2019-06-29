15198ffd6b6903d144ceac8225b4b60af4121fa5
zachmolony <31472522+zachmolony@users.noreply.github.com>
2019-06-30 00:22:39 +0100

    added firebase for back end

---
+++ darklab/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';  
+  
+// Your web app's Firebase configuration
+var firebaseConfig = {
+apiKey: "AIzaSyAAyKZa1cYpSi95uq_ZMeFLf-xErgFHGHs",
+authDomain: "darklab2037.firebaseapp.com",
+databaseURL: "https://darklab2037.firebaseio.com",
+projectId: "darklab2037",
+storageBucket: "",
+messagingSenderId: "588342541848",
+appId: "1:588342541848:web:a46e4fa8f705f241"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
