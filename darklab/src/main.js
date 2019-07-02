33a65cbc7646afed6887b92e7361b08ddfe6e680
zachmolony <31472522+zachmolony@users.noreply.github.com>
2019-07-02 21:06:20 +0100

    created vuex store

--- darklab/src/main.js
+++ darklab/src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+
 import App from './App.vue'
 import router from './router'
+import store from './store.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(Vuex)
