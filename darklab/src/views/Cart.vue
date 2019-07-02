54bc33f51bae56b790a7329e4f934314a434f1b0
zachmolony <31472522+zachmolony@users.noreply.github.com>
2019-07-02 23:04:18 +0100

    began designing reciept

--- darklab/src/views/Cart.vue
+++ darklab/src/views/Cart.vue
@@ -2,7 +2,17 @@
     <div>
         <Navbar class="navbar" />
         <div class="basket">
-          <h1 v-if="basketIsEmpty">You have nothing in your cart.</h1>
+          <h1 v-if="basket.length == 0">You have nothing in your cart.</h1>
+          <div class="item-container" v-else :key="index" v-for="(item, index) in basket">
+            <div class="item">
+              <img class="product-img" v-bind:src="item.img">
+              <div class="details">
+                <h1>{{item.title}}</h1>
+                <p>{{item.price}}</p>
+              </div>
+              <p>x {{item.price}}</p>
+            </div>
+          </div>
         </div>
     </div>
 </template>
@@ -24,36 +34,56 @@ export default {
 </script>
 v-for="(item, index) in basket"
 <style scoped>
+h1 {
+  font-size: 1em;
+}
+
 .basket {
   width: 80vw;
   margin-top: 20px;
   margin-left: auto;
   margin-right: auto;
-  max-width: 450px;
+  max-width: 400px;
   border: 1px;
   border-style: solid;
   border-radius: 5px;
   border-color: rgb(187, 187, 187);
+  padding: 15px;
 }
 
 .item-container {
   display: flex;
   flex-direction: column;
+  margin-bottom: 20px;
 }
 
 .item {
   display: flex;
+  justify-content: space-between;
 }
 
 .product-img {
   width: auto;
   height: 20vw;
-  max-height: 100px;
+}
+
+.details {
+  text-align: left;
+  margin-top: -15px;
+  margin-left: -30px;
 }
 
   @media screen and (min-width: 500px) {
     h1 {
       font-size: 26px;
+    }
+    .product-img {
+      width: auto;
+      height: 100px;
+    }
+    .details {
+      margin-top: -20px;
+      margin-left: -80px;
     }
   }
 </style>
