3ca2f2a67e9662179f501d25d242a6e9c7186154
zachmolony <31472522+zachmolony@users.noreply.github.com>
2019-10-27 23:16:01 +0000

    set index to enter page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,52 @@
 <template>
-  <div id="app" >
-    <img id="background" src="../assets/sky.gif" alt="">
-    <router-view class="page_content" />
-  </div>
+    <div>
+        <div id="header">
+            <div class="skull">
+                <img src="../assets/skull.gif" alt="">
+            </div>
+            <img id="logo" src="../assets/logo-rough.png" alt="">
+            <nuxt-link to="/home">
+                <button class="enterButton">enter store.</button>
+            </nuxt-link>
+        </div>
+    </div>
 </template>
 
 <script>
-  import Navbar from '../components/Navbar'
-
-  export default {
-
-  }
+export default {
+    name: 'Enter'
+}
 </script>
 
+<style scoped>
+  #header {
+    position: absolute;
+    padding-top: 28vh;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+  }
 
-<style>
+  .skull * {
+    max-width: 130px;
+    padding-bottom: 2vh;
+  }
+  
+#logo {
+    height: 30px;
+    width: auto;
+}
+
+.enterButton {
+    margin-top: 4vh;
+    background-color: transparent;
+    color: red;
+}
 </style>
